85cb200b951e1cb409cfedb2d0f703c06ff3453b
VsevolodsKr <kronbergseva@gmail.com>
2024-05-14 19:43:50 +0300

    Installed Tailwind

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>
+    <h1 class="font-bold text-xl">
         hellow 
     </h1>
 </template>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         @vite(['resources/js/app.js'])
+        @vite('resources/css/app.css')
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
