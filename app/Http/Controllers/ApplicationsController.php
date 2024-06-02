f6dc97d663b71599bde475f929fc3a7da198acc3
VsevolodsKr <kronbergseva@gmail.com>
2024-06-02 12:54:10 +0300

    Created Controller and Model for Applications

---
+++ app/Http/Controllers/ApplicationsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Applications;
+use Illuminate\Http\Request;
+
+class ApplicationsController extends Controller
+{
+    //
+}
---
+++ app/Models/Applications.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Applications extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'surname',
+        'birth_date',
+        'phone',
+        'email',
+        'comments',
+        'status',
+    ];
+}
