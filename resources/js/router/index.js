e48259131c96284c76b2e59ad65c7501df3c755b
Sofia Dyshlovaya <sofija45642@gmail.com>
2024-06-13 13:26:34 +0300

    Safe routes

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -12,32 +12,72 @@ const routes = [
     {
         path: "/",
         component: Main,
+        meta: { guest: true },
     },
     {
         path: "/login",
         component: Login,
+        meta: { guest: true },
     },
     {
         path: "/about",
         component: About,
+        meta: { guest: true },
     },
     {
         path: "/vacancies",
         component: Vacancies,
+        meta: { guest: true },
     },
     {
         path: "/news",
         component: News,
+        meta: { guest: true },
     },
     {
         path: "/admin",
         component: AdminPanel,
+        meta: { requiresAuth: true },
     },
-    { path: "/vacancies/:id", component: Vacancy },
-    { path: "/news/:id", component: New },
+    { path: "/vacancies/:id", component: Vacancy, meta: { guest: true } },
+
+    { path: "/news/:id", component: New, meta: { guest: true } },
 ];
+
+// Function to check if user is logged in
+function loggedIn() {
+    return localStorage.getItem("token");
+}
+
 const router = createRouter({
     history: createWebHistory(),
     routes,
 });
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some((record) => record.meta.requiresAuth)) {
+        // Redirect to login if not authenticated
+        if (!loggedIn()) {
+            next({
+                path: "/login",
+                query: { redirect: to.fullPath },
+            });
+        } else {
+            next();
+        }
+    } else if (to.matched.some((record) => record.meta.guest)) {
+        // Redirect to dashboard if already logged in
+        if (loggedIn()) {
+            next({
+                path: "/admin",
+                query: { redirect: to.fullPath },
+            });
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+});
+
 export default router;
