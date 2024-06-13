c939b9b95f64e3ea39efc01561e5529182d4e4b0
Sofia Dyshlovaya <sofija45642@gmail.com>
2024-06-13 13:10:52 +0300

    Minor fixes

--- resources/js/components/Block.vue
+++ resources/js/components/Block.vue
@@ -1,7 +1,11 @@
 <template>
     <div
         class="ring-1 ring-gray-200 rounded-xl py-5 px-8 flex flex-col justify-between items-center gap-5 text-black dark:text-gray-50 bg-white dark:bg-zinc-700 shadow-2xl max-w-md hover:ring-2 transition-all ">
-        <h1 class="text-2xl font-bold underline">{{ data.title }}</h1>
+        <!-- <h1 class="text-2xl font-bold underline">{{ data.title }}</h1> -->
+        <div class="flex justify-center">
+            <h1 class="font-bold text-xl text-center bg-calm-green rounded-xl shadow-md p-4 text-gray-50">
+                {{ data.title }}</h1>
+        </div>
         <div class="min-[600px]:w-96 min-w-48 h-48 overflow-hidden rounded-md relative shadow-2xl ">
             <img :src="data.image_path" class="-translate-y-1/2 absolute top-1/2">
         </div>
@@ -13,6 +17,7 @@
 </template>
 
 <script>
+import { data } from 'autoprefixer';
 import CustomButton from './CustomButton.vue';
 export default {
     props: ['data', 'btnTitle', 'route'],
--- resources/js/pages/separate-view/New.vue
+++ resources/js/pages/separate-view/New.vue
@@ -2,13 +2,17 @@
     <Wrapper>
         <div class="flex flex-wrap gap-5">
             <img v-if="current" :src="this.current.image_path"
-                class="max-w-96 mr-16 rounded-xl shadow-md dark:ring-1 dark:ring-zinc-700">
+                class="max-w-96 mr-16 rounded-xl shadow-2xl dark:ring-1 dark:ring-zinc-700">
             <div v-if="current"
-                class="bg-white ring-1 max-w-6xl ring-gray-300 rounded-md shadow-xl py-10 px-7 dark:bg-zinc-700">
-                <h1 v-if="current" class="font-bold text-3xl mx-16 text-black dark:text-gray-50 mb-5">{{
-                    current.title
-                    }}
-                </h1>
+                class="bg-white ring-1 max-w-6xl ring-gray-300 rounded-2xl shadow-xl py-10 px-7 dark:bg-zinc-700">
+
+                <div v-if="current" class="mb-5 flex justify-center">
+                    <h1 class="font-bold text-3xl text-center bg-calm-green rounded-xl shadow-md p-4 text-gray-50">
+                        {{
+                            current.title
+                        }}</h1>
+
+                </div>
                 <p class="text-justify leading-7 indent-16 text-black dark:text-gray-50">{{ current.description }}
                 </p>
             </div>
